61f28aaa7eceb7027b6795608a216b6f55ab7bc4
Cathal Mac Donnacha <macdonnacha.cathal@gmail.com>
2020-04-14 19:28:49 +0100

    Updates

---
+++ precache-manifest.320e5672f9714173147b2ee5be7e6911.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "01ed56a9bf810e2e768335fa795814c9",
+    "url": "/cmacdonnacha/react-boilerplate/index.html"
+  },
+  {
+    "revision": "63a2588dab5d7e5cb8da",
+    "url": "/cmacdonnacha/react-boilerplate/static/js/2.ba4eb6c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cmacdonnacha/react-boilerplate/static/js/2.ba4eb6c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "633fd0e86004b661bcb2",
+    "url": "/cmacdonnacha/react-boilerplate/static/js/main.b7000af4.chunk.js"
+  },
+  {
+    "revision": "e0915dcece4c22fefbcd",
+    "url": "/cmacdonnacha/react-boilerplate/static/js/runtime-main.aea92e93.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cmacdonnacha/react-boilerplate/precache-manifest.320e5672f9714173147b2ee5be7e6911.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cmacdonnacha/react-boilerplate/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
